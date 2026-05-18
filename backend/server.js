5380733a0bdf19f3fa1a661aadcdb9b0f8ee8a09
Soumendra-Barick <soumendrabarick86@gmail.com>
2026-05-18 09:07:37 +0530

    Initial commit

---
+++ backend/server.js
@@ -0,0 +1,44 @@
+require("dotenv").config();
+const app = require("./src/app");
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const generateResponse = require("./src/service/ai.service");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors:{
+    origin:"http://localhost:5173"
+  }
+});
+
+const chatHistory = [];
+
+io.on("connection", (socket) => {
+  console.log("A user is Connected");
+
+  socket.on("disconnect", () => {
+    console.log("A user disconnected");
+  });
+
+  socket.on("ai-message", async (data) => {
+    console.log("AI message Received:", data);
+
+    chatHistory.push({
+      role: "user",
+      parts: [{ text: data }],
+    });
+
+    const response = await generateResponse(chatHistory);
+
+    chatHistory.push({
+      role: "model",
+      parts: [{ text: response }],
+    });
+
+    socket.emit("ai-message-response", response);
+  });
+});
+
+httpServer.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
---
+++ frontend/vite-project/src/App.jsx
@@ -0,0 +1,136 @@
+import { useState, useRef, useEffect } from "react";
+import { io } from "socket.io-client";
+import "./App.css";
+
+function App() {
+  const [socket, setSocket] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [input, setInput] = useState("");
+  const messagesEndRef = useRef(null);
+
+  useEffect(() => {
+    messagesEndRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  }, [messages]);
+
+  const handleSendMessage = () => {
+    if (input.trim() === "") return;
+
+    const userMessage = {
+      id: Date.now(),
+      text: input,
+      sender: "user",
+      time: new Date().toLocaleTimeString([], {
+        hour: "2-digit",
+        minute: "2-digit",
+      }),
+    };
+
+    setMessages((prev) => [...prev, userMessage]);
+
+    const userInput = input;
+    socket.emit("ai-message", input);
+    setInput("");
+
+    setTimeout(() => {
+      const botMessage = {
+        id: Date.now() + 1,
+        text: `✨ AI Response`,
+        sender: "bot",
+        time: new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+      };
+
+      setMessages((prev) => [...prev, botMessage]);
+    }, 700);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleSendMessage();
+    }
+  };
+
+  useEffect(() => {
+    let socketInstance = io("http://localhost:3000");
+    setSocket(socketInstance);
+
+    socketInstance.on("ai-message-response", (response) => {
+      const botMessage = {
+        id: Date.now() + 1,
+        text: response,
+        timestamp: new Date().toLocaleTimeString(),
+        sender: "bot",
+      };
+      setMessages((prev) => [...prev, botMessage]);
+    });
+  }, []);
+
+  return (
+    <div className="app">
+      <div className="chat-box">
+        {/* Header */}
+        <div className="header">
+          <div>
+            <h1>Nova AI</h1>
+            <p>Your Smart Assistant</p>
+          </div>
+
+          <div className="status">
+            <span></span>
+            Online
+          </div>
+        </div>
+
+        {/* Messages */}
+        <div className="messages">
+          {messages.length === 0 ? (
+            <div className="welcome">
+              <h2>👋 Welcome</h2>
+              <p>Start chatting with your AI assistant</p>
+            </div>
+          ) : (
+            messages.map((msg) => (
+              <div
+                key={msg.id}
+                className={`message-row ${
+                  msg.sender === "user" ? "user-row" : "bot-row"
+                }`}
+              >
+                <div
+                  className={`message ${
+                    msg.sender === "user" ? "user-message" : "bot-message"
+                  }`}
+                >
+                  <p>{msg.text}</p>
+                  <span>{msg.time}</span>
+                </div>
+              </div>
+            ))
+          )}
+
+          <div ref={messagesEndRef}></div>
+        </div>
+
+        {/* Input */}
+        <div className="input-area">
+          <textarea
+            placeholder="Ask anything..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            onKeyDown={handleKeyDown}
+            rows="2"
+          />
+
+          <button onClick={handleSendMessage}>➤</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
