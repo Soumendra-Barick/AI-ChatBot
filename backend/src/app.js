55bf1f61ca99e141760e94682319994f23e206ef
Soumendra-Barick <soumendrabarick86@gmail.com>
2026-05-18 11:32:04 +0530

    Remove unused Hello World route from app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,10 +3,6 @@ const path = require("path");
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
-
 app.use(express.static(path.join(__dirname, "../public")));
 
 // Catch-all route
